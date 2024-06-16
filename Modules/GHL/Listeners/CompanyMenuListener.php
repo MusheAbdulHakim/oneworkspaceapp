22ebf3a5a4931fd7687a6d655be18cb326d83c11
musheabdulhakim <musheabdulhakim99@gmail.com>
2024-06-16 00:37:41 +0000

    menu updated

--- Modules/GHL/Listeners/CompanyMenuListener.php
+++ Modules/GHL/Listeners/CompanyMenuListener.php
@@ -14,11 +14,11 @@ class CompanyMenuListener
         $module = 'GHL';
         $menu = $event->menu;
         $menu->add([
-            'category' => 'Erp-operation',
+            'category' => 'General',
             'title' => __('GoHighLevel'),
             'icon' => 'location',
-            'name' => 'gohigh',
-            'parent' => 'erp-operations',
+            'name' => 'ghl',
+            'parent' => 'operations',
             'order' => 1,
             'ignore_if' => [],
             'depend_on' => [],
@@ -27,16 +27,96 @@ class CompanyMenuListener
             // 'permission' => 'hrm manage'
         ]);
         $menu->add([
-            'title' => __('Dashboard'),
+            'category' => 'General',
+            'title' => __('GoHighLevel Dashboard'),
             'icon' => '',
             'name' => 'ghl-dashboard',
-            'parent' => 'gohigh',
-            'order' => 1,
+            'parent' => 'dashboard',
+            'order' => 30,
             'ignore_if' => [],
             'depend_on' => [],
             'route' => 'ghl.dashboard',
             'module' => $module,
+            // 'permission' => ''
+        ]);
+        $menu->add([
+            'category' => 'General',
+            'title' => __('Contacts'),
+            'icon' => '',
+            'name' => 'ghl-contacts',
+            'parent' => 'ghl',
+            'order' => 1,
+            'ignore_if' => [],
+            'depend_on' => [],
+            'route' => 'ghl.contacts',
+            'module' => $module,
+            // 'permission' => ''
+        ]);
+        $menu->add([
+            'category' => 'General',
+            'title' => __('Calendars'),
+            'icon' => '',
+            'name' => 'ghl-calendars',
+            'parent' => 'ghl',
+            'order' => 2,
+            'ignore_if' => [],
+            'depend_on' => [],
+            'route' => 'ghl.calendars',
+            'module' => $module,
             'permission' => ''
         ]);
+        $menu->add([
+            'category' => 'General',
+            'title' => __('Calendars Events'),
+            'icon' => '',
+            'name' => 'ghl-calendars-events',
+            'parent' => 'ghl',
+            'order' => 2,
+            'ignore_if' => [],
+            'depend_on' => [],
+            'route' => 'ghl.calendars.events',
+            'module' => $module,
+            'permission' => ''
+        ]);
+        $menu->add([
+            'category' => 'General',
+            'title' => __('Funnels'),
+            'icon' => '',
+            'name' => 'ghl-funnels',
+            'parent' => 'ghl',
+            'order' => 2,
+            'ignore_if' => [],
+            'depend_on' => [],
+            'route' => 'ghl.funnels',
+            'module' => $module,
+            'permission' => ''
+        ]);
+        $menu->add([
+            'category' => 'General',
+            'title' => __('Invoices'),
+            'icon' => '',
+            'name' => 'ghl-invoices',
+            'parent' => 'ghl',
+            'order' => 3,
+            'ignore_if' => [],
+            'depend_on' => [],
+            'route' => 'ghl.invoices',
+            'module' => $module,
+            'permission' => ''
+        ]);
+        $menu->add([
+            'category' => 'General',
+            'title' => __('Campaigns'),
+            'icon' => '',
+            'name' => 'ghl-campaigns',
+            'parent' => 'ghl',
+            'order' => 3,
+            'ignore_if' => [],
+            'depend_on' => [],
+            'route' => 'ghl.campaigns',
+            'module' => $module,
+            'permission' => ''
+        ]);
+
     }
 }
