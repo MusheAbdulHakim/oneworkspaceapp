c909b9a660e173de2f8b39842baa9b4bb7495773
musheabdulhakim <musheabdulhakim99@gmail.com>
2024-06-18 07:31:03 +0000

    updated

--- resources/views/bookmarks/card.blade.php
+++ resources/views/bookmarks/card.blade.php
@@ -36,12 +36,12 @@
         @else
         <a href="{{ $bookmark->url }}" target="_blank">
         @endif
-            <div class="card text-white" style="background-color: {{ $bookmark->color ?? '#fff' }}">
+            <div class="card" style="background-color: {{ $bookmark->color ?? '#fff' }}">
                 <div class="card-body">
                     @if (!empty($bookmark->image))
                     <div class="theme-avtar">
                         <img src="{{ get_file(('uploads/bookmarks/'.$bookmark->image)) }}"
-                            alt={{ $bookmark->url }}" style="max-width: 100%;">
+                            alt="site image" style="max-width: 100%;">
                     </div>
                     @endif
                     <h6 class="card-header">{{ $bookmark->title }}</h6>
