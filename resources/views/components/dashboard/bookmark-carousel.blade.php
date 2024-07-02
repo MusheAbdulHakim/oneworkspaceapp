0662ab9e4750a49400b646ded0ef6fb8539c035c
musheabdulhakim <musheabdulhakim99@gmail.com>
2024-07-02 10:36:29 +0000

    initial commit

---
+++ resources/views/components/dashboard/bookmark-carousel.blade.php
@@ -0,0 +1,81 @@
+@php
+    $bookmarks = \App\Models\UrlBookmark::where('status', '1')->orderBy('order')->get();
+@endphp
+@if(!empty($bookmarks) && $bookmarks->count() > 0)
+<div class="col-md-9 col-12">
+    <p class="text-black"><b> URL Bookmarks</b></p>
+</div>
+<div class="col-md-3 col-12">
+    <a href="{{ route('bookmark.create') }}" data-bs-toggle="tooltip"
+        data-title="{{ __('Add Bookmark') }}"
+        data-bs-original-title="{{ __('Add Bookmark') }}"
+        class="btn btn-sm btn-primary btn-icon">
+        <i class="ti ti-plus"></i> Add Bookmark
+    </a>
+</div>
+<div class="bookmark-carousel">
+    @foreach ($bookmarks as $i => $bookmark)
+    <div class="col-sm-3 col-12 product-card">
+        <div class="card rounded-0">
+            <div class="checkbox-custom">
+                <div class="btn-group card-option float-end">
+                    <button type="button" class="btn p-0" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <i class="ti ti-dots-vertical"></i>
+                    </button>
+                    <div class="dropdown-menu dropdown-menu-end">
+                        <a class="dropdown-item" data-title="{{ __('Edit Bookmark') }}"
+                            href="{{ route('bookmark.edit', $bookmark->id) }}"
+                            data-toggle="tooltip">
+                            <span><i class="ti ti-edit"></i> Edit</span>
+                        </a>
+                        {{ Form::open(['route' => ['bookmark.destroy', $bookmark->id], 'class' => 'm-0']) }}
+                        @method('DELETE')
+                        <a href="#!" class="dropdown-item bs-pass-para show_confirm" aria-label="Delete"
+                            data-confirm="{{ __('Are You Sure?') }}"
+                            data-text="{{ __('This action can not be undone. Do you want to continue?') }}"
+                            data-confirm-yes="delete-form-{{ $bookmark->id }}">
+                            <span><i class="ti ti-trash"></i> Delete</span>
+                        </a>
+                        {{ Form::close() }}
+                    </div>
+                </div>
+            </div>
+            @if ($bookmark->type == '1')
+            <a href="{{ route('bookmark.iframe', ['urlBookmark' => \Crypt::encrypt($bookmark)]) }}">
+            @else
+                <a href="{{ $bookmark->url }}" target="_blank">
+            @endif
+            @if (!empty($bookmark->image))
+            <div class="card-body p-2 m-2">
+                <div class="d-flex align-items-center justify-content-center">
+                    <div class="theme-avtar">
+                        <img src='{{ asset("storage/bookmarks/".$bookmark->image) }}'
+                        style="max-width: 100%;" class="img-user">
+                    </div>
+                </div>
+                <p class="text-center">{{ $bookmark->title }}</p>
+            </div>
+            @else
+            <div class="product-img justify-content-center my-3">
+                <p class="text-cente">{{ $bookmark->title }}</p>
+            </div>
+            @endif
+            </a>
+        </div>
+    </div>
+    @endforeach
+</div>
+<script>
+    $(document).ready(function(){        
+        $('.bookmark-carousel').slick({
+            infinite: true,
+            lazyLoad: 'ondemand',
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            draggable: true,
+            prevArrow: '<button type="button" class="btn btn-sm customPrev btn-primary"><span class="material-symbols-outlined">chevron_left</span></button>',
+            nextArrow: '<button type="button" class="btn btn-sm customNext btn-primary"><span class="material-symbols-outlined">chevron_right</span></button>',
+        });   
+    });
+</script>
+@endif
---
+++ resources/views/components/dashboard/slick-addons.blade.php
@@ -0,0 +1,71 @@
+@if (!empty(ActivatedModule()) && count(ActivatedModule()) > 0)
+
+    <p class="text-black my-3"><b>Addon Manager</b></p>
+    <div class="slick-addon">
+        @php
+            $exceptions = ['ProductService', 'Stripe', 'Paypal'];
+            $icon_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        @endphp
+        @foreach (ActivatedModule() as $i => $module)
+            <div>
+                @php
+                    $module = Module::find($module);
+                    $i += 1;
+                @endphp
+                @if (!in_array($module, $exceptions))
+                    <div data-bs-toggle="tooltip" data-bs-original-title="{{ $module->getName() }}">
+                        <a href="{{ route('software.details', Module_Alias_Name($module->getName())) }}" target="_new">
+                            <div class="card rounded">
+                                <div class="card-body p-2 m-2">
+                                    <div class="d-flex align-items-center justify-content-center">
+                                        <div class="">
+                                            @php
+                                                $src = asset("images/widgets/$i.png");
+                                                $capitalizeName = strtoupper($module->getName());
+                                            @endphp
+                                            @if (in_array($i, $icon_numbers))
+                                                @php
+                                                    if($capitalizeName == 'GHL'){
+                                                        $src = asset('images/widgets/GoHighlevel.png');
+                                                    }
+                                                    if($capitalizeName == 'HRM'){
+                                                        $src = asset('images/widgets/HRM.png');
+                                                    }
+                                                    if($capitalizeName == 'Lead'){
+                                                        $src = asset('images/widgets/HRM.png');
+                                                    }
+                                                @endphp
+                                                <img src='{{ $src }}'
+                                                    alt="{{ $capitalizeName }}" style="max-width: 100%;"
+                                                    class="img-user">
+                                            @else
+                                            <img src='{{ $src }}'
+                                                alt="{{ $capitalizeName }}" style="max-width: 100%;"
+                                                class="img-user">
+                                            @endif
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                @endif
+            </div>
+        @endforeach
+    </div>
+        
+    <script>
+        $(document).ready(function() {
+            $('.slick-addon').slick({
+                infinite: true,
+                lazyLoad: 'ondemand',
+                slidesToShow: 6,
+                slidesToScroll: 1,
+                draggable: true,
+                prevArrow: '<button type="button" class="btn btn-sm customPrev btn-primary"><span class="material-symbols-outlined">chevron_left</span></button>',
+                nextArrow: '<button type="button" class="btn btn-sm customNext btn-primary"><span class="material-symbols-outlined">chevron_right</span></button>',
+            })
+        })
+    </script>
+@endif  
+
